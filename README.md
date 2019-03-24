fb7a1a4fbebec0d0ab2b267e1bb38d3f0bb25cd6
Ken Yang <ken.yang@wdc.com>
2019-03-24 17:50:36 +0800

    1. initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+** sfdisk-arm installation:
+1. Install arm-linux-gnueabihf-gcc. Make sure it is added to $PATH
+2. ./build_sfdisk-arm.sh
+3. sfdisk.static will be in $INSTALL_PATH/sbin
+
---
+++ build_sfdisk-arm.sh
@@ -0,0 +1,20 @@
+INSTALL_PATH="$(pwd -P)/install/arm"
+TOP_PATH="$(pwd -P)"
+echo "INSTALL_PATH=$INSTALL_PATH"
+sleep 3
+cd ${TOP_PATH}
+cd ncurses-6.1
+rm -Rf .build
+mkdir -p .build
+cd .build
+../configure --host=arm-linux-gnueabihf --enable-widec --prefix=$INSTALL_PATH
+make;make install
+cd ${TOP_PATH}
+cd util-linux-2.33
+rm -Rf .build
+mkdir -p .build
+cd .build
+../configure --prefix=$INSTALL_PATH  --host=arm-linux-gnueabihf CFLAGS=-I${INSTALL_PATH}/include LDFLAGS=-L${INSTALL_PATH}/lib --enable-static-programs=fdisk,sfdisk --enable-libfdisk --enable-libuuid --without-python --without-systemd  --enable-libblkid --disable-makeinstall-chown
+make;make install
+
+
